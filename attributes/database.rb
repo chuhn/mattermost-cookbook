0bd8cc628507f16f717ffb673a0ec07301cdf492
Christopher Huhn <C.Huhn@gsi.de>
2021-01-25 18:25:53 +0100

    Individual attributes for database setup with 'default_unless' priority

--- attributes/database.rb
+++ attributes/database.rb
@@ -1,7 +1,5 @@
-default['mattermost']['database'] = {
-  'address' => 'dockerhost',
-  'port' => 3306,
-  'name' => 'mattermost_test',
-  'username' => 'mmuser',
-  'password' => 'mostest',
-}
+default_unless['mattermost']['database']['address']  = 'dockerhost'
+default_unless['mattermost']['database']['port']     = 3306
+default_unless['mattermost']['database']['name']     = 'mattermost_test'
+default_unless['mattermost']['database']['username'] = 'mmuser'
+default_unless['mattermost']['database']['password'] = 'mostest'
