d9980fb161ba39fd303502f00e33955d9d9521bf
Christopher Huhn <C.Huhn@gsi.de>
2021-01-25 21:08:01 +0100

    Disentangle default['mattermost']['config'] options

--- attributes/aa_mattermost.rb
+++ attributes/aa_mattermost.rb
@@ -3,13 +3,11 @@ default['mattermost']['package'] = {
   'checksum' => '2da727da93b0d193eb3dfdfadb2534eb43dbbf68bce84074d3cc89619bb8f263',
 }
 
-default['mattermost']['config'] = {
-  'install_path' => '/opt',
-  'user' => 'mattermost',
-  'group' => 'mattermost',
-  'data_dir' => '/opt/mattermost/data',
-  'server_name' => 'localhost',
-}
+default['mattermost']['config']['install_path'] = '/opt'
+default['mattermost']['config']['user']         = 'mattermost'
+default['mattermost']['config']['group']        = 'mattermost'
+default['mattermost']['config']['data_dir']     = '/opt/mattermost/data'
+default['mattermost']['config']['server_name']  = 'localhost'
 
 default['mattermost']['config']['path'] =
   default['mattermost']['config']['install_path'] + '/mattermost/config/config.json'
