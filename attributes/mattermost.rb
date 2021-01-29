d272187ed637be3e5298d6bcf76c739a8167bfe3
Christopher Huhn <C.Huhn@gsi.de>
2021-01-29 09:58:01 +0100

    default → default_unless

--- attributes/mattermost.rb
+++ attributes/mattermost.rb
@@ -1,13 +1,13 @@
-default['mattermost']['version'] = '5'
-default['mattermost']['edition'] = 'team'
+default_unless['mattermost']['version'] = '5'
+default_unless['mattermost']['edition'] = 'team'
 
-default['mattermost']['config']['install_path'] = '/opt'
-default['mattermost']['config']['user']         = 'mattermost'
-default['mattermost']['config']['group']        = node['mattermost']['config']['user']
-default['mattermost']['config']['data_dir']     = '/opt/mattermost/data'
-default['mattermost']['config']['server_name']  = 'localhost'
+default_unless['mattermost']['config']['install_path'] = '/opt'
+default_unless['mattermost']['config']['user']         = 'mattermost'
+default_unless['mattermost']['config']['group']        = node['mattermost']['config']['user']
+default_unless['mattermost']['config']['data_dir']     = '/opt/mattermost/data'
+default_unless['mattermost']['config']['server_name']  = 'localhost'
 
-default['mattermost']['config']['path'] =
+default_unless['mattermost']['config']['path'] =
   node['mattermost']['config']['install_path'] + '/mattermost/config/config.json'
 
-default['mattermost']['systemd']['after'] = %w( syslog.target network.target )
+default_unless['mattermost']['systemd']['after'] = %w( syslog.target network.target )
