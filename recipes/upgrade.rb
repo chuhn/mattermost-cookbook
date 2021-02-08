729308aae674c7a9912057a34ad55648c07c6cd6
Christopher Huhn <C.Huhn@gsi.de>
2021-02-08 19:43:38 +0100

    Remove old stuff before upgrade

--- recipes/upgrade.rb
+++ recipes/upgrade.rb
@@ -10,12 +10,16 @@ execute 'systemctl stop mattermost'
 url = node['mattermost']['packages'][node['mattermost']['edition']][wanted_version]['url']
 checksum = node['mattermost']['packages'][node['mattermost']['edition']][wanted_version]['checksum']
 
+directory "node['mattermost']['config']['install_path']/mattermost" do
+  action: :delete
+end
+
 tar_extract url do
   download_dir node['mattermost']['config']['install_path']
   target_dir node['mattermost']['config']['install_path']
   checksum checksum
-  user node['mattermost']['config']['user']
-  group node['mattermost']['config']['user']
+  # user node['mattermost']['config']['user']
+  # group node['mattermost']['config']['user']
   action :extract
 end
 
